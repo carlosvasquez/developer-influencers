da3dac2f3bd5b9c9d32515c52e9e873a4ad7a467
Carlos Vasquez Polanco <carlosslim@gmail.com>
2014-10-28 02:21:04 -0400

    definición de menú y selección de opción

--- src/main/java/org/devdom/influencer/bean/AdministrationController.java
+++ src/main/java/org/devdom/influencer/bean/AdministrationController.java
@@ -26,10 +26,17 @@ public class AdministrationController implements Serializable {
     private String viewId;
     private final HttpSession session;
 
+    /**
+     * 
+     * @param request 
+     */
     public AdministrationController(HttpServletRequest request) {
         session = request.getSession();
     }
 
+    /**
+     * 
+     */
     public AdministrationController(){
         facesContext = FacesContext.getCurrentInstance();
         externalContext = facesContext.getExternalContext();
@@ -80,6 +87,10 @@ public class AdministrationController implements Serializable {
         }
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String getLastViewId(){
         String lastViewID = (String) session.getAttribute("lastViewID");
         String paramString = "";
@@ -95,15 +106,65 @@ public class AdministrationController implements Serializable {
 
         return lastViewID+paramString;
     }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getCSSStyleHeaderMenu(){
 
-    public String getViewx(){
-        return viewId;
+        String currentPage = getViewId();
+        String outcome = "<ul class=\"top-ul-menu\">"; 
+
+        String[][] pages = {{"index.xhtml","Inicio"},
+                            {"groups.xhtml","Grupos"},
+                            {"skills.xhtml","Skills"},
+                            {"university.xhtml","Universidades"},
+                            {"workplace.xhtml","Plazas de Trabajo"},};
+
+        int idx = 1;
+        for(String[] page : pages){
+            outcome += getPageOption(currentPage,page[0],page[1]);
+
+            if(idx<pages.length){
+                outcome += "<li class=\"menu-top-sep\">&nbsp;/&nbsp;</li>";
+            }
+            idx++;
+        }
+        outcome += "</ul>";
+
+        return outcome;
+    }
+
+    /**
+     * 
+     * @param active
+     * @param link
+     * @param text
+     * @return 
+     */
+    private String getPageOption(String active, String link, String text){
+        String styleClass;
+        if(active.endsWith(link)){
+            styleClass = "menu-top-selected";
+        }else{
+            styleClass =  "menu-top";
+        }
+        return "<li class=\""+styleClass+"\"><a href=\""+link+"\">"+text+"</a></li>";
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String getViewId(){
         return viewId;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public boolean isHomePage(){
         return viewId.endsWith("index.xhtml");
     }
